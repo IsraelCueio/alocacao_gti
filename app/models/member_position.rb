ffda7502b1f028151ec1eea5380b27874b53bf0f
gtijr-acer <gtijr@gti.com>
2020-10-10 14:53:57 -0300

    Fix callbacks

--- app/models/member_position.rb
+++ app/models/member_position.rb
@@ -2,8 +2,9 @@ class MemberPosition < ApplicationRecord
   belongs_to :member
   belongs_to :position
 
-  after_commit :recalc_member_load
-
+  after_destroy :recalc_member_load
+  after_create :recalc_member_load
+  
   def recalc_member_load
     member.calc_member_load
   end
--- app/models/member_project.rb
+++ app/models/member_project.rb
@@ -3,6 +3,7 @@ class MemberProject < ApplicationRecord
   belongs_to :project
 
   after_destroy :recalc_member_load
+  after_create :recalc_member_load
 
   def recalc_member_load
     member.calc_member_load
