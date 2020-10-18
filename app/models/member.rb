a9760652fc492d8f2c7160d081215f0418366e4d
Gustavo Castro <lgx.0097@gmail.com>
2020-10-18 00:31:35 -0300

    ⬆️ Update member load logic

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,6 +10,75 @@ class Member < ApplicationRecord
   accepts_nested_attributes_for :member_positions, reject_if: :all_blank, allow_destroy: true
 
   def calc_member_load
-    update(load: positions.sum(:load) + projects.sum(:load))
+    update(load: calc_development_load + calc_managing_load + calc_position_load)
+  end
+
+  def calc_position_load
+    positions.sum(:load)
+  end
+
+  def calc_development_load
+    projects_load = 0
+    projects.each do |project|
+      projects_load += calc_project_development_load(project)
+    end
+    projects_load
+  end
+
+  def calc_managing_load
+    projects_load = 0
+    managing_projects.each do |project|
+      projects_load += calc_project_managing_load(project)
+    end
+    projects_load
+  end
+
+  def calc_project_development_load(project)
+    project_type_load(project) * complexity_factor(project) * state_factor(project)
+  end
+
+  def calc_project_managing_load(project)
+    project_type_load(project) * complexity_factor(project) * state_factor(project)
+  end
+
+  def project_type_load(project)
+    case project.type_project
+    when 'Site' then 20
+    when 'Woocommerce' then 25
+    when 'Consulting' then 25
+    when 'System' then 30
+    else 1
+    end
+  end
+
+  def complexity_factor(project)
+    case project.complexity
+    when 'Beginner' then 1.0
+    when 'Intermediate' then 1.5
+    when 'Experient' then 2.0
+    else 1
+    end
+  end
+
+  def state_factor(project)
+    case project.state
+    when 'Stoped' then 0.1
+    when 'Starting' then 1
+    when 'Middle' then 1
+    when 'Finishing' then 1
+    when 'Support' then 0.3
+    else 1
+    end
+  end
+
+  def manager_state_factor(project)
+    case project.state
+    when 'Stoped' then 0.1
+    when 'Starting' then 1.0
+    when 'Middle' then 0.5
+    when 'Finishing' then 1.0
+    when 'Support' then 0.1
+    else 1
+    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,4 @@
 class Project < ApplicationRecord
-  before_save :calc_project_load
   after_save :update_members_loads
 
   enum type_project: %i[Site Woocommerce Consulting System]
@@ -17,37 +16,4 @@ class Project < ApplicationRecord
     members.each(&:calc_member_load)
   end
 
-  def calc_project_load
-    self.load = project_type_load * complexity_factor * state_factor
-  end
-
-  def project_type_load
-    case type_project
-    when 'Site' then 1
-    when 'Woocommerce' then 2
-    when 'Consulting' then 2
-    when 'System' then 4
-    else 1
-    end
-  end
-
-  def complexity_factor
-    case complexity
-    when 'Beginner' then 1
-    when 'Intermediate' then 2
-    when 'Experient' then 3
-    else 1
-    end
-  end
-
-  def state_factor
-    case state
-    when 'Stoped' then 0.2
-    when 'Starting' then 1
-    when 'Middle' then 1
-    when 'Finishing' then 1
-    when 'Support' then 0.5
-    else 1
-    end
-  end
 end
