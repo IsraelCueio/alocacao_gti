c4a8c79c888363eb2fa31b5d71ec0d69f72f0a09
Gustavo Castro <lgx.0097@gmail.com>
2020-11-16 19:13:25 -0300

    💫 Project and dashboard translations

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def complexity_enum_select_options(keys)
+    keys.map { |key| [t("enums.project.complexity.#{key}"), key] }
+  end
+  
+  def state_enum_select_options(keys)
+    keys.map { |key| [t("enums.project.state.#{key}"), key] }
+  end
 end
