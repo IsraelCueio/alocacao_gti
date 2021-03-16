84f1107fe7471f80bd57b7d3d770fe972c5f1174
IsraelCueio <israelcoelhocunhaccc@gmail.com>
2021-03-16 20:56:01 -0300

    new_send_grid_account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD']
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY']
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
